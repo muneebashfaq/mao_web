dd67fe5d48ee50a85150e7463eae3a1ea23bdae1
unknown <mmuneeb840@gmail.com>
2022-04-15 16:51:15 +0500

    mao_website

--- src/Components/Navbars.jsx
+++ src/Components/Navbars.jsx
@@ -3,56 +3,84 @@ import { NavLink } from "react-router-dom";
 import './css/Navbar.css'
 const Navbars = () => {
     return (<>
-    <div className="list">
-        <div className="container">
-        <div className="row">
-            <div className="col-12 d-none d-lg-block">
-                <ul className="list">
-                    <li className="list"><NavLink exact to="/" className={isActive =>
-    "active" + (!isActive ? " unselected" : "")
-  }>Home</NavLink></li>
-                    <li class="list"> <NavLink exact activeClassName="active" to="/principal_page">Principal's Page</NavLink></li>
-                    <li class="list"> <NavLink to="/administration">Administration</NavLink></li>
-                    <li class="list"> <NavLink to="/faculties">Faculties</NavLink></li>
-                    <li class="list"> <NavLink to="/admissions">Admissions</NavLink></li>
-                    <li class="list"> <NavLink to="/gallery">Gallery</NavLink></li>
-                    <li class="list"> <NavLink to="/notices">Notices</NavLink></li>
-                    <li class="list"> <NavLink to="/contact_us">Contact_us</NavLink></li>
-                </ul>
-            </div>
-            <div className="col-12 d-lg-none d-block ">
+        <div className="list">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 d-none d-lg-block">
+                        <ul className="list">
+                            <li className="list"><NavLink exact to="/" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Home</NavLink></li>
+                            <li class="list"> <NavLink exact className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            } to="/principal_page">Principal's Page</NavLink></li>
+                            <li class="list"> <NavLink to="/administration" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Administration</NavLink></li>
+                            <li class="list"> <NavLink to="/faculties" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Faculties</NavLink></li>
+                            <li class="list"> <NavLink to="/admissions" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Admissions</NavLink></li>
+                            <li class="list"> <NavLink to="/gallery" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Gallery</NavLink></li>
+                            <li class="list"> <NavLink to="/notices" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Notices</NavLink></li>
+                            <li class="list"> <NavLink to="/contact_us" className={isActive =>
+                                "active" + (!isActive ? " unselected" : "")
+                            }>Contact_us</NavLink></li>
+                        </ul>
+                    </div>
+                    <div className="col-12 d-lg-none d-block ">
 
 
-                <div class="navbar navbar-custom">
-                    <div class="container">
-                        <a class="navbar-brand" href="/">
+                        <div class="navbar navbar-custom">
+                            <div class="container">
+                                <a class="navbar-brand" href="/">
 
-                        </a>
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">☰</button>
-                        <div class="navbar-collapse collapse">
-                            <ul class="nav navbar-nav">
+                                </a>
+                                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target=".navbar-collapse">☰</button>
+                                <div class="navbar-collapse collapse">
+                                    <ul class="nav navbar-nav">
 
-                                <li class="dropdown menu-large nav-item">
-                                </li>
-                                <li class="list2"><NavLink className={isActive =>
-    "active" + (!isActive ? " active" : "")
-  } to="/">Home</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/principal_page">Principal's Page</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/administration">Administration</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/faculties">Faculties</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/admissions">Admissions</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/gallery">Gallery</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/notices">Notices</NavLink><hr /></li>
-                                <li class="list2"> <NavLink to="/contact_us">Contact_us</NavLink></li>
-                            </ul>
+                                        <li class="dropdown menu-large nav-item">
+                                        </li>
+                                        <li class="list2"><NavLink className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        } to="/">Home</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/principal_page" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Principal's Page</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/administration" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Administration</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/faculties" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Faculties</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/admissions" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Admissions</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/gallery" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Gallery</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/notices" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Notices</NavLink><hr /></li>
+                                        <li class="list2"> <NavLink to="/contact_us" className={isActive =>
+                                            "active" + (!isActive ? " active" : "")
+                                        }>Contact_us</NavLink></li>
+                                    </ul>
 
 
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        </div>
         </div>
     </>)
 }
